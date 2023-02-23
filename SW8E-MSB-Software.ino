c50a2ccdd83e1230789e02ec1abbbe6899678109
William Kelso <wpkelso@ncsu.edu>
2023-02-22 21:53:57 -0500

    Reformatted the entire file

--- SW8E-MSB-Software.ino
+++ SW8E-MSB-Software.ino
@@ -4,49 +4,35 @@
 
 //-------------------------------
 // GLOBAL VARIABLES
-Servo torpedo_servo1, torpedo_servo2;
 //-------------------------------
+Servo torpedo_servo1, torpedo_servo2;
 
+//-------------------------------
 // SETUP FUNCTIONS
 //-------------------------------
-//-------------------------------
-// DROPPER SETUP FUNCTION
 void dropper_setup() {
   digitalWrite(DROP1_CTRL, HIGH);
   digitalWrite(DROP2_CTRL, HIGH);
 }
-//-------------------------------
 
-//-------------------------------
-// TORPEDO SERVO SETUP FUNCTION
 void torpedo_servo_setup() {
   torpedo_servo1.attatch(SERVO1_CTRL);
   torpedo_servo2.attatch(SERVO2_CTRL);
 }
-//-------------------------------
 
-//-------------------------------
-// FULL BOARD SETUP FUNCTION
 void setup() {
   dropper_setup();
   torpedo_servo_setup();
 }
-//-------------------------------
-//-------------------------------
 
+//-------------------------------
 // GENERAL RUNTIME TASK FUNCTIONS
 //-------------------------------
-//-------------------------------
-// 
 void task_torpedo_servo() {
   
 }
-//-------------------------------
 
-//-------------------------------
-//
 void loop() {
   
 }
-//-------------------------------
 //-------------------------------
